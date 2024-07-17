1c6e919e0f1934e54abb16273497e9d4be2375f8
kaufman79 <kaufman79@gmail.com>
2024-07-17 14:05:25 -0400

    Home->Menu in navbar. Add some bootstrap classes. Add icon in navbar. etc

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='home'),  # the class in views.py
+    path('', views.MenuList.as_view(), name='menu'),  # the class in views.py
     path('menuitem/<int:pk>/', views.MenuItemDetail.as_view(), name='menu_item'),
     path('about/', views.AboutView.as_view(), name='about')
 ]
